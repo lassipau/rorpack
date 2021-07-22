bd2a5f7c2183dcde5085bd07c3b91ae49fb35a4e
Jonne Karru (TAU) <jonne.karru@tuni.fi>
2021-07-22 08:41:46 +0300

    Timoshenko construction now equivalent with Matlab.

---
+++ examples/TimoshenkoLHMNC18.py
@@ -0,0 +1,86 @@
+'''
+Robust output tracking of a passive Timoshenko beam model with distributed inputs and outputs.
+
+Originally written for the conference presentation at Lagrangian and Hamiltonian Methods in
+Nonlinear Control (LHMNC) in Valparaiso, Chile in 2018.
+
+The simulation is associated to the conference paper by Paunonen, Le Gorrec, and Ramirez at LHMNC 2018
+(the conference paper does not include the simulation).
+
+The controller is a "simple passive controller" studied in the conference paper
+(also see Rebarber-Weiss 2003).
+
+The system has collocated distributed inputs and outputs,
+and in the simulation it is approximated using a Finite Differences scheme.
+'''
+
+import numpy as np
+from scipy.sparse import spdiags, bmat, eye, lil_matrix
+from rorpack.system import LinearSystem
+from rorpack.controller import *
+from rorpack.closed_loop_system import ClosedLoopSystem
+from rorpack.plotting import *
+
+def construct_TimoshenkoLHMNC18(w0fun, wd0fun, phi0fun, phid0fun, N):
+    h = 1/N
+
+    bw = 2
+    bphi = 2
+    xis = np.linspace(0,1,N+1)
+
+    # The simulation uses values \rho=1 and I_\rho=1 (hard-coded in operators
+    # A, B, C).
+    rho = 1
+    I_rho = 1
+
+    ee = np.ones((1,N))
+
+    P012 = (1/h*spdiags(np.vstack((-ee, ee)), [-1, 0], N, N)).todense()
+    P021 = (1/h*spdiags(np.vstack((-ee, ee)), [0, 1], N, N)).todense()
+
+    A11 = np.bmat([[np.zeros((N, N)), P012], [P021, -bw*np.eye(N)]])
+    A22 = np.bmat([[np.zeros((N, N)), P012], [P021, -bphi*np.eye(N)]])
+
+    P114 = spdiags(np.vstack((-ee, 0*ee)), [-1, 0], N, N).todense()
+    P141 = -P114.T
+
+    ZN = np.zeros((N, N))
+
+    A12 = np.bmat([ZN, P114], [ZN, ZN])
+    A21 = np.bmat([ZN, ZN], [P141, ZN])
+
+    A = np.bmat([[A11, A12], [A21, A22]])
+
+    # Control and observation on [4/5 1], both on the state variable x_4(t)
+    B = np.bmat([[np.zeros((3*N, 1))], [np.atleast_2d(xis[1:] >= 0.8).T]])
+    C = h*B.T
+
+    Bd = np.zeros((4*N, 1))
+    D = np.zeros((1, 1))
+
+    spgrid = xis
+
+    # Compute the initial state based on w0fun, wd0fun, phi0, and phid0
+    # x0 = [diff(w0fun(xis))/h-phi0fun(xis(1:(end-1)));rho*wd0fun(xis(2:end));diff(phi0fun(xis))/h;I_rho*phid0fun(xis(2:end))];
+    x0 = np.bmat([[np.diff(w0fun(xis))/h - phi0fun(xis[:-1])], [rho*wd0fun(xis[1:])], [np.diff(phi0fun(xis))], [phid0fun(xis[1:])]])
+
+    return LinearSystem(A, B, C, D, Bd), spgrid, x0
+
+
+# Parameters for this example.
+N = 50
+
+# Initial state of the plant (the state has four components, x_1, ... x_4)
+# The initial state is based on the initial data for w(xi,t) and phi(xi,t),
+# namely w0fun (initial deflection), wd0fun (initial velocity), 
+# phi0fun (initial angular displacement), phid0fun (initial angular
+# velocity)
+
+w0fun = lambda x: np.cos(np.pi*x)
+# wd0fun = lambda x: np.zeros(np.atleast_1d(x).shape)
+wd0fun = lambda x: 5*np.sin(np.pi*x)
+phi0fun = lambda x: np.zeros(np.atleast_1d(x).shape)
+phid0fun = lambda x: np.zeros(np.atleast_1d(x).shape)
+
+# Construct the Timoshenko beam model from the conference article by
+# Paunonen, Le Gorrec and Ramirez at LHMNC in 2018.
